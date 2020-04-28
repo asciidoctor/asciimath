5aa643e39b0a7a335810f949aaa2c310dcd0ede9
Pablo Emilio Escobar Gaviria <pablo-escobar@riseup.net>
2020-04-28 11:11:32 -0300

    Finished implementing unary, binary and trinary operators

--- lib/asciimath/latex.rb
+++ lib/asciimath/latex.rb
@@ -3,155 +3,151 @@ module AsciiMath
     SPECIAL_CHARACTERS = [?&, ?%, ?$, ?#, ?_, ?{, ?}, ?~, ?^, ?[, ?]]
 
     CONSTANTS = {
-        "α" => "\\alpha",
-        "β" => "\\beta",
-        "γ" => "\\gamma",
-        "Γ" => "\\Gamma",
-        "δ" => "\\delta",
-        "Δ" => "\\Delta",
-        "ε" => "\\epsilon",
-        "ɛ" => "\\varepsilon",
-        "ζ" => "\\zeta",
-        "η" => "\\eta",
-        "θ" => "\\theta",
-        "Θ" => "\\Theta",
-        "ϑ" => "\\vartheta",
-        "ι" => "\\iota",
-        "κ" => "\\kappa",
-        "λ" => "\\lambda",
-        "Λ" => "\\Lambda",
-        "μ" => "\\mu",
-        "ν" => "\\nu",
-        "ξ" => "\\xi",
-        "Ξ" => "\\Xi",
-        "π" => "\\pi",
-        "Π" => "\\Pi",
-        "ρ" => "\\rho",
-        "σ" => "\\sigma",
-        "Σ" => "\\Sigma",
-        "τ" => "\\tau",
-        "υ" => "\\upsilon",
-        "ϕ" => "\\phi",
-        "Φ" => "\\Phi",
-        "φ" => "\\varphi",
-        "χ" => "\\chi",
-        "ψ" => "\\psi",
-        "Ψ" => "\\Psi",
-        "ω" => "\\omega",
-        "Ω" => "\\Omega",
-        "∅" => "\\emptyset",
-        "∞" => "\\infty",
-        "ℵ" => "\\aleph"
+        ?α => "\\alpha",
+        ?β => "\\beta",
+        ?γ => "\\gamma",
+        ?Γ => "\\Gamma",
+        ?δ => "\\delta",
+        ?Δ => "\\Delta",
+        ?ε => "\\epsilon",
+        ?ɛ => "\\varepsilon",
+        ?ζ => "\\zeta",
+        ?η => "\\eta",
+        ?θ => "\\theta",
+        ?Θ => "\\Theta",
+        ?ϑ => "\\vartheta",
+        ?ι => "\\iota",
+        ?κ => "\\kappa",
+        ?λ => "\\lambda",
+        ?Λ => "\\Lambda",
+        ?μ => "\\mu",
+        ?ν => "\\nu",
+        ?ξ => "\\xi",
+        ?Ξ => "\\Xi",
+        ?π => "\\pi",
+        ?Π => "\\Pi",
+        ?ρ => "\\rho",
+        ?σ => "\\sigma",
+        ?Σ => "\\Sigma",
+        ?τ => "\\tau",
+        ?υ => "\\upsilon",
+        ?ϕ => "\\phi",
+        ?Φ => "\\Phi",
+        ?φ => "\\varphi",
+        ?χ => "\\chi",
+        ?ψ => "\\psi",
+        ?Ψ => "\\Psi",
+        ?ω => "\\omega",
+        ?Ω => "\\Omega",
+        ?∅ => "\\emptyset",
+        ?∞ => "\\infty",
     }
 
     OPERATORS = {
-        "⋅"    => "cdot",
-        "∗"    => "ast",
-        "⋆"    => "star",
-        "\\"   => "backslash",
-        "×"    => "times",
-        "÷"    => "div",
-        "⋉"    => "ltimes",
-        "⋊"    => "rtimes",
-        "⋈"    => "bowtie",
-        "∘"    => "circ",
-        "⊕"    => "oplus",
-        "⊗"    => "otimes",
-        "⊙"    => "odot",
-        "∑"    => "Sigma",
-        "∏"    => "Pi",
-        "∧"    => "wedge",
-        "⋀"    => "bigwedge",
-        "∨"    => "vee",
-        "⋁"    => "bigvee",
-        "∩"    => "cap",
-        "⋂"    => "bigcap",
-        "∪"    => "cup",
-        "⋃"    => "bigcup",
-        "≠"    => "neq",
-        "≤"    => "leq",
-        "≥"    => "geq",
-        "≺"    => "prec",
-        "⪯"    => "preceq",
-        "≻"    => "succ",
-        "⪰"    => "succeq",
-        "∈"    => "in",
-        "∉"    => "notin",
-        "⊂"    => "subset",
-        "⊃"    => "supset",
-        "⊆"    => "subseteq",
-        "⊇"    => "supseteq",
-        "≡"    => "equiv",
-        "≅"    => "cong",
-        "≈"    => "approx",
-        "∝"    => "propto",
-        "∫"    => "int", 
-        "∮"    => "oint",
-        "∂"    => "partial",
-        "∇"    => "nabla",
-        "±"    => "pm",
-        "∴"    => "therefore",
-        "∵"    => "because",
+        ?⋅     => "cdot",
+        ?∗     => "ast",
+        ?⋆     => "star",
+        ?\\    => "backslash",
+        ?×     => "times",
+        ?÷     => "div",
+        ?⋉     => "ltimes",
+        ?⋊     => "rtimes",
+        ?⋈     => "bowtie",
+        ?∘     => "circ",
+        ?⊕     => "oplus",
+        ?⊗     => "otimes",
+        ?⊙     => "odot",
+        ?∑     => "Sigma",
+        ?∏     => "Pi",
+        ?∧     => "wedge",
+        ?⋀     => "bigwedge",
+        ?∨     => "vee",
+        ?⋁     => "bigvee",
+        ?∩     => "cap",
+        ?⋂     => "bigcap",
+        ?∪     => "cup",
+        ?⋃     => "bigcup",
+        ?ℵ     => "aleph",
+        ?≠     => "neq",
+        ?≤     => "leq",
+        ?≥     => "geq",
+        ?≺     => "prec",
+        ?⪯     => "preceq",
+        ?≻     => "succ",
+        ?⪰     => "succeq",
+        ?∈     => "in",
+        ?∉     => "notin",
+        ?⊂     => "subset",
+        ?⊃     => "supset",
+        ?⊆     => "subseteq",
+        ?⊇     => "supseteq",
+        ?≡     => "equiv",
+        ?≅     => "cong",
+        ?≈     => "approx",
+        ?∝     => "propto",
+        ?∫     => "int", 
+        ?∮     => "oint",
+        ?∂     => "partial",
+        ?∇     => "nabla",
+        ?±     => "pm",
+        ?∴     => "therefore",
+        ?∵     => "because",
         "..."  => "ldots",
-        "⋯"    => "cdots",
-        "⋮"    => "vdots",
-        "⋱"    => "ddots",
-        "∠"    => "angle",
-        "⌢"    => "frown",
-        "△"    => "triangle",
-        "⋄"    => "diamond",
-        "□"    => "square",
-        "⌊"    => "lfloor",
-        "⌋"    => "rfloor",
-        "⌈"    => "lceil",
-        "⌉"    => "rceil",
-        "ℂ"    => "mathbb{C}",
-        "ℕ"    => "mathbb{N}",
-        "ℚ"    => "mathbb{Q}",
-        "ℝ"    => "mathbb{R}",
-        "ℤ"    => "mathbb{Z}",
-        "¬"    => "neg",
-        "⇒"    => "Rightarrow",
-        "⇔"    => "Leftrightarrow",
-        "∀"    => "forall",
-        "∃"    => "exists",
-        "⊥"    => "bot",
-        "⊤"    => "top",
-        "⊢"    => "vdash",
-        "⊨"    => "models",
-        "↑"    => "uparrow",
-        "↓"    => "downarrow",
-        "→"    => "rightarrow",
-        "↣"    => "rightarrowtail",
-        "↠"    => "twoheadrightarrow",
-        "⤖"    => "twoheadrightarrowtail",
-        "↦"    => "mapsto",
-        "←"    => "leftarrow",
-        "↔"    => "leftrightarrow",
-        "⇐"    => "Leftarrow",
-        "¯"    => "widebar",
-        :frac  => "frac",
-        :sqrt  => "sqrt",
-        :over  => "overset",
-        :under => "underset"
+        ?⋯     => "cdots",
+        ?⋮     => "vdots",
+        ?⋱     => "ddots",
+        ?∠     => "angle",
+        ?⌢     => "frown",
+        ?△     => "triangle",
+        ?⋄     => "diamond",
+        ?□     => "square",
+        ?⌊     => "lfloor",
+        ?⌋     => "rfloor",
+        ?⌈     => "lceil",
+        ?⌉     => "rceil",
+        ?ℂ     => "mathbb{C}",
+        ?ℕ     => "mathbb{N}",
+        ?ℚ     => "mathbb{Q}",
+        ?ℝ     => "mathbb{R}",
+        ?ℤ     => "mathbb{Z}",
+        ?¬     => "neg",
+        ?⇒     => "Rightarrow",
+        ?⇔     => "Leftrightarrow",
+        ?∀     => "forall",
+        ?∃     => "exists",
+        ?⊥     => "bot",
+        ?⊤     => "top",
+        ?⊢     => "vdash",
+        ?⊨     => "models",
+        ?↑     => "uparrow",
+        ?↓     => "downarrow",
+        ?→     => "rightarrow",
+        ?↣     => "rightarrowtail",
+        ?↠     => "twoheadrightarrow",
+        ?⤖     => "twoheadrightarrowtail",
+        ?↦     => "mapsto",
+        ?←     => "leftarrow",
+        ?↔     => "leftrightarrow",
+        ?⇐     => "Leftarrow",
+        ?¯     => "widebar",
     }
 
     PARENS = {
-        "(" => "(",
-        ")" => ")",
-        "[" => "\\[",
-        "]" => "\\]",
-        "{" => "\\{",
-        "}" => "\\}",
-        "⟨" => "\\langle",
-        "⟩" => "\\rangle",
-        "⌊" => "\\lfloor",
-        "⌋" => "\\rfloor",
-        "⌈" => "\\lceil",
-        "⌉" => "\\rceil",
-        "|" => "|",
-        "∥" => "\\parallel",
-        nil => "."
+        ?(  => ?(,
+        ?)  => ?),
+        ?[  => ?[,
+        ?]  => ?],
+        ?{  => "\\{",
+        ?}  => "\\}",
+        ?⟨  => "\\langle",
+        ?⟩  => "\\rangle",
+        ?⌊  => "\\lfloor",
+        ?⌋  => "\\rfloor",
+        ?⌈  => "\\lceil",
+        ?⌉  => "\\rceil",
+        ?|  => ?|,
+        ?∥  => "\\parallel",
+        nil => ?.,
     }
 
     FONTS = {
@@ -218,27 +214,18 @@ module AsciiMath
               end
             when :unary
               identifier = expression[:identifier]
-              operator = expression[:operator]
               if identifier
-                operation(expression[:operator], identifier)
+                unary(expression[:operator], identifier)
               else
-                operation(expression[:operator], expression[:s])
+                unary(expression[:operator], expression[:s])
               end
             when :binary
-              s1 = expression[:s1]
-              s2 = expression[:s2]
-
-              if s2.is_a?(Hash) and s2.has_key? :underover
-                operation(expression[:operator], s1, s2)
-              else
-                operation(expression[:operator], s2, s1)
-              end
+              binary(expression[:operator], expression[:s1], expression[:s2])
             when :ternary
-              operator = expression[:operator]
               s1 = expression[:s1]
               s2 = expression[:s2]
-              s2 = expression[:s3]
-              operation(expression[:operator], s1, s2, s3)
+              s3 = expression[:s3]
+              ternary(expression[:operator], s1, s2, s3)
             when :matrix
               rows = expression[:rows]
               len = rows.length - 1
@@ -258,7 +245,7 @@ module AsciiMath
     end
 
     def method_missing(meth, *args, &block)
-      macrocall(meth, *args, &block)
+      macro(meth, *args, &block)
     end
 
     def parens(lparen, rparen, &block)
@@ -272,7 +259,7 @@ module AsciiMath
     end
 
     def font(font, &block)
-        macrocall(FONTS[font], &block)
+        macro(FONTS[font], &block)
     end
 
     def operation(operator, *args)
@@ -289,13 +276,121 @@ module AsciiMath
         end
     end
 
-    def macrocall(macro, *args)
-      text = args.last.is_a?(String) ? args.pop : ''
+    def unary(operator, s1)
+      case operator
+      when :sqrt
+        sqrt do
+          append(s1)
+        end
+      else
+        macro(operator) do
+          append(s1)
+        end
+      end
+    end
 
-      @latex << "\\#{macro}{ "
+    def binary(operator, s1, s2)
+      case operator
+      when :sub
+        @latex << "{ "
+        append(s1)
+        @latex << " }_{ "
+        append(s2)
+        @latex << " }"
+      when :sup
+        @latex << "{ "
+        append(s1)
+        @latex << " }^{ "
+        append(s2)
+        @latex << " }"
+      when :frac
+        operation("frac", s1, s2)
+      when :root
+        sqrt(s1) do
+          append(s2)
+        end
+      when :over
+        if not s2.is_a?(Hash)
+          raise "Unimplemented"
+        end
 
-      if block_given? || text
-        append_escaped(text)
+        case s2[:c]
+        when "^"
+          hat do
+            append(s1)
+          end
+        when "¯"
+          overline do
+            append(s1)
+          end
+        when "→"
+          vec do
+            append(s1)
+          end
+        when "."
+          dot do
+            append(s1)
+          end
+        when ".."
+          ddot do
+            append(s1)
+          end
+        when "⏞"
+          overbrace do
+            append(s1)
+          end
+        else
+          operation("overset", s1, s2)
+        end
+      when :under
+        if not s2.is_a?(Hash)
+          raise "Unimplemented"
+        end
+
+        case s2[:c]
+        when "_"
+          underline do
+            append(s1)
+          end
+        when "⏟"
+          underbrace do
+            append(s1)
+          end
+        else
+          operation("underset", s1, s2)
+        end
+      else
+        operation(operator, s1, s2)
+      end
+    end
+
+    def ternary(operator, s1, s2, s3)
+      case operator
+      when :subsup
+        @latex << "{ "
+        append(s1)
+        @latex << " }_{ "
+        append(s2)
+        @latex << " }^{ "
+        append(s3)
+        @latex << " }"
+      else
+        operation(operator, s1, s2, s3)
+      end
+    end
+
+    def macro(macro, *args)
+      @latex << "\\#{macro}"
+
+      if args.length
+        @latex << "["
+        append(args, ", ")
+        @latex << "]"
+      end
+
+      @latex << "{ "
+
+      if block_given?
         yield self if block_given?
       end
 
