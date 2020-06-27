ef51c3fedd2457717a54d8b04171170c402832c0
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2020-06-27 17:14:04 +0200

    Add some customisation test cases

---
+++ spec/customisation_spec.rb
@@ -0,0 +1,28 @@
+#encoding: utf-8
+require 'rspec'
+require 'asciimath'
+require 'ast'
+
+RSpec.configure do |c|
+  include ::AsciiMath::ASTHelper
+end
+
+describe 'AsciiMath::Parser', :variant => :ast do
+  it "should support custom symbols" do
+    my_tokens_table = AsciiMath::SymbolTableBuilder.new
+    AsciiMath::Parser.add_default_parser_symbols(my_tokens_table)
+    my_tokens_table.add('mysymbol', :mysymbol, :symbol)
+
+    parsed = AsciiMath::parse("a + mysymbol + b", my_tokens_table.build)
+    expect(parsed.ast).to eq(seq(identifier('a'), symbol('+'), ::AsciiMath::AST::Symbol.new(:mysymbol, 'mysymbol'), symbol('+'), identifier('b')))
+  end
+
+  it "should support replacing standard symbols" do
+    my_tokens_table = AsciiMath::SymbolTableBuilder.new
+    AsciiMath::Parser.add_default_parser_symbols(my_tokens_table)
+    my_tokens_table.add('+', :foo, :symbol)
+
+    parsed = AsciiMath::parse("a + b", my_tokens_table.build)
+    expect(parsed.ast).to eq(seq(identifier('a'), ::AsciiMath::AST::Symbol.new(:foo, '+'), identifier('b')))
+  end
+end
